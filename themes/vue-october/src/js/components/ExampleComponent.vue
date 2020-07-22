f93bbeb792310d3b3bd174e15d63fd2a4285be54
Darren <darrenjevon@gmail.com>
2020-07-21 22:43:00 -0400

    first commit

---
+++ themes/vue-october/src/js/components/ExampleComponent.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="card card-default">
+          <div class="card-header">{{ title }}</div>
+
+          <div class="card-body">
+            You did it D!
+            <p>{{ message }}</p>
+            <span class="person">{{ person }}</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    person: {
+      type: String,
+      default: null
+    }
+  },
+  data() {
+    return {
+      title: "My first Vue Component",
+      message: "Awesome Job Setting up Vue in October!"
+    };
+  },
+  mounted() {
+    console.log("Component mounted.");
+  }
+};
+</script>
