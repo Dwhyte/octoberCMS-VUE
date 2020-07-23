2432bb921db3f5df44ecbfebd55563a4e5ebd867
Darren <darrenjevon@gmail.com>
2020-07-22 21:06:02 -0400

    updated example vue component

--- themes/vue-october/src/js/components/ExampleComponent.vue
+++ themes/vue-october/src/js/components/ExampleComponent.vue
@@ -9,6 +9,11 @@
             You did it D!
             <p>{{ message }}</p>
             <span class="person">{{ person }}</span>
+            <ul>
+              <li v-for="user in fakeData" :key="user.id">
+                <h5>{{ user.name }}</h5>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
@@ -21,17 +26,32 @@ export default {
   props: {
     person: {
       type: String,
-      default: null
-    }
+      default: null,
+    },
   },
   data() {
     return {
       title: "My first Vue Component",
-      message: "Awesome Job Setting up Vue in October!"
+      message: "Awesome Job Setting up Vue in October!",
+      fakeData: null,
     };
   },
   mounted() {
     console.log("Component mounted.");
-  }
+    this.getFakeJson();
+  },
+  methods: {
+    async getFakeJson() {
+      try {
+        let res = await axios.get("https://jsonplaceholder.typicode.com/users");
+        if (res.data) {
+          this.fakeData = res.data;
+          console.log(res.data);
+        }
+      } catch (error) {
+        console.log(error);
+      }
+    },
+  },
 };
 </script>
